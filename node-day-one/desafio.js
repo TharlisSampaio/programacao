099c960ea80d74327850b314a0f9bcfd8f787e20
TharlisSampaio <tharlis.sampaio@outlook.com>
2022-11-11 20:24:00 -0500

    aula 11/11/22

--- node-day-one/desafio.js
+++ node-day-one/desafio.js
@@ -1,11 +1,11 @@
 fetch('http://jsonplaceholder.typicode.com/users')
     .then(T => T.json())
     .then((dado)=> (dado.forEach(item => {
-        const ul = document.querySelector('#text')
+        // const ul = document.querySelector('#text')
 
         const li = document.createElement('li')
         li.textContent = `nome: ${item['name']} e-mail: ${item.email}`
-        ul.appendChild(li)
-        console.log(ul)
+        document.querySelector('#text').appendChild(li)
+        console.log(document.querySelector('#text').appendChild(li))
 
     })))
